0acf2435d73690e94757de6dd007687f56c30ab7
plettj <jlplett@uwaterloo.ca>
2024-10-20 14:46:52 +0200

    fix: URLs

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,9 @@ export const BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://plett.dev"
     : "http://localhost:3000";
+
+export const URL_HOME = "/";
+export const URL_WRITING = "/writing";
+export const URL_PHOTOGRAPHY = "/photography";
+export const URL_MEDIA = "/media";
+export const URL_CV = "/cv";
