faa6eceaf4b9a5e1614d03dfc0ed8f7ce5e137d4
plettj <jlplett@uwaterloo.ca>
2024-10-23 04:32:19 +0200

    fix: Accurate 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,22 +4,32 @@ import BackButton from "@/components/BackButton";
 import { Button } from "@/components/ui/button";
 import { URL_HOME } from "@/lib/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NotFound() {
+  const pathname = usePathname();
+
   return (
-    <div className="w-full h-full p-8 items-center justify-center text-center">
-      <span className="bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
-        404
-      </span>
-      <p className="my-4">
-        Sorry, the page you are looking for doesn&apos;t exist or has been
-        moved.
-      </p>
-      <div className="mt-8 flex justify-center gap-2">
-        <BackButton variant="outline" />
-        <Button variant="ghost" asChild>
-          <Link href={URL_HOME}>Back to Home</Link>
+    <div className="w-full h-full p-8 flex items-center justify-center text-center">
+      <div>
+        <span className="bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
+          404
+        </span>
+        <p className="mt-4 mb-8">
+          The page you&apos;re looking for either doesn&apos;t exist or has been
+          moved.
+        </p>
+        <Button asChild>
+          <Link href={`https://old.plett.dev${pathname}`}>
+            Try the old site
+          </Link>
         </Button>
+        <div className="mt-8 flex justify-center gap-2">
+          <BackButton variant="outline" />
+          <Button variant="ghost" asChild>
+            <Link href={URL_HOME}>Back to Home</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
