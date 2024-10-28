7a62c8bdbc6a33b9fac92cf7d52698d8a168b3b6
plettj <jlplett@uwaterloo.ca>
2024-10-28 23:55:18 +0100

    fix: Group public IP addresses

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { incrViews } from "./actions/middleware/views";
-import ipaddr from "ipaddr.js";
+import { processIp } from "./lib/utils";
 
 // TODO: Improve my matcher.
 export const config = {
@@ -13,13 +13,11 @@ export default async function middleware(
 ) {
   const response = NextResponse.next();
 
-  const forwarded = request.headers.get("x-forwarded-for");
-  const ip = forwarded ? forwarded.split(/, /)[0] : request.ip ?? "unknown";
-
-  // Ensure public IP addresses are not counted.
-  if (ipaddr.parse("192.168.5.1").range() !== "private") {
-    return response;
-  }
+  const forwardedIp = request.headers.get("x-forwarded-for");
+  const rawIp = forwardedIp
+    ? forwardedIp.split(/, /)[0]
+    : request.ip ?? "unknown";
+  const ip = processIp(rawIp);
 
   await incrViews(ip, context, ["global_views"]);
 
