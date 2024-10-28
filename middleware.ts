a6e87d28fcec95bde4d3f4775527debb49fcc970
plettj <jlplett@uwaterloo.ca>
2024-10-28 21:48:18 +0100

    update: Remove matcher

--- middleware.ts
+++ middleware.ts
@@ -3,29 +3,14 @@ import { incrViews } from "./actions/middleware/views";
 
 // TODO: Improve my matcher.
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.png (favicon file)
-     */
-    {
-      source: "/((?!api|_next/static|_next/image|favicon.png).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" },
-      ],
-    },
-  ],
-};
-
 export default async function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
-  incrViews(request.ip ?? "unknown", ["global_views"]);
+  const ip = request.headers.get("x-forwarded-for") || request.ip || "unknown";
+
+  console.log("Middleware running on IP: ", ip);
+
+  incrViews(ip, ["global_views"]);
 
   return response;
 }
