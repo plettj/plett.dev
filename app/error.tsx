2dc9f30ce410b3a5990a5657e1dff8d592df91ab
plettj <jlplett@uwaterloo.ca>
2024-11-02 20:45:46 +0100

    feat: Error boundaries

---
+++ app/error.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import BackButton from "@/components/BackButton";
+import Container from "@/components/layouts/Container";
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Container>
+      <div className="items-center justify-center text-center">
+        <span className="bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
+          :(
+        </span>
+        <p className="mt-8 mb-8">Something went wrong in your browser.</p>
+        <div className="flex justify-center gap-2">
+          <BackButton variant="ghost" />
+          <Button variant="ghost" onClick={() => reset()}>
+            Try again
+          </Button>
+        </div>
+      </div>
+    </Container>
+  );
+}
