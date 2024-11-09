a35d93025dfe243b0f6d63ebe9b66314b4938829
plettj <jlplett@uwaterloo.ca>
2024-11-09 04:30:05 +0100

    style: Misc polishes

--- components/ProfilePhoto.tsx
+++ components/ProfilePhoto.tsx
@@ -15,42 +15,46 @@ type ProfilePhotoProps = {
 
 export default function ProfilePhoto({ src, size }: ProfilePhotoProps) {
   const [isLoaded, setIsLoaded] = useState(false);
-  const { theme } = useTheme();
+  const { theme, setTheme } = useTheme();
   const [activeImage, setActiveImage] = useState(0);
 
   useEffect(() => {
     setActiveImage(theme === "dark" ? 1 : 0);
   }, [theme]);
 
+  const handleClick = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   const images = Array.isArray(src) ? src : [src, src];
   const sizes = Array.isArray(size) ? size : [size, size];
 
   return (
-    <div className="relative border rounded-full overflow-hidden size-36 sm:size-32 aspect-square flex-grow-0 flex-shrink-0">
-      {images.map((imageSrc, index) => {
-        if (index === 1 && !isLoaded) return null;
-        return (
-          <Image
-            key={index}
-            src={imageSrc}
-            title={`${index === 1 ? "Dark" : "Light"} Profile Photo | ${
-              AUTHOR.name
-            }`}
-            alt={`${index === 1 ? "Dark" : "Light"} Profile Photo | ${
-              AUTHOR.name
-            }`}
-            width={sizes[index]}
-            height={sizes[index]}
-            priority
-            draggable={false}
-            onLoad={() => setIsLoaded(true)}
-            className={cn(
-              "absolute inset-0 transition-all duration-200 rounded-full pointer-events-none",
-              isLoaded && activeImage === index ? "opacity-100" : "opacity-0"
-            )}
-          />
-        );
-      })}
+    <div
+      className="relative border rounded-full overflow-hidden size-36 sm:size-32 aspect-square flex-grow-0 flex-shrink-0"
+      onClick={handleClick}
+    >
+      {images.map((imageSrc, index) => (
+        <Image
+          key={index}
+          src={imageSrc}
+          aria-label={`${index === 1 ? "Dark" : "Light"} Profile Photo | ${
+            AUTHOR.name
+          }`} // aria-label instead of title to prevent tooltip on hover.
+          alt={`${index === 1 ? "Dark" : "Light"} Profile Photo | ${
+            AUTHOR.name
+          }`}
+          width={sizes[index]}
+          height={sizes[index]}
+          priority
+          draggable={false}
+          onLoad={() => setIsLoaded(true)}
+          className={cn(
+            "absolute inset-0 transition-all duration-200 rounded-full cursor-pointer",
+            isLoaded && activeImage === index ? "opacity-100" : "opacity-0"
+          )}
+        />
+      ))}
     </div>
   );
 }
