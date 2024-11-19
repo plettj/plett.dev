c4331be22bf9ded1b0c2afa795e0050cbcf85763
plettj <jlplett@uwaterloo.ca>
2024-11-19 16:38:40 +0100

    upgrade: ESLint v9

--- components/MouseTooltip.tsx
+++ components/MouseTooltip.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Tippy from "@tippyjs/react/headless";
-import { followCursor } from "tippy.js/headless";
+import React from "react";
 
 export default function MouseTooltip({
   tooltip,
@@ -10,26 +9,18 @@ export default function MouseTooltip({
   tooltip: React.ReactNode;
   children: React.ReactNode;
 }) {
-  return (
-    <Tippy
-      render={(attrs) => (
-        <>
-          <div className="size-12 bg-red-500">TESTER SQUARE</div>
-          <div
+  /* <div
             tabIndex={-1}
             className="max-w-56 border bg-background px-2 py-1.5 text-xs rounded-md"
             {...attrs}
           >
             {tooltip}
-          </div>
-        </>
-      )}
-      animation="fade"
-      followCursor="horizontal"
-      trigger="mouseenter focus"
-      plugins={[followCursor]}
-    >
-      <>{children}</>
-    </Tippy>
+          </div> */
+
+  return (
+    <>
+      {tooltip}
+      {children}
+    </>
   );
 }
