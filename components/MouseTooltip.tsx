5088785078602984cd63109da760270e80fe7435
plettj <jlplett@uwaterloo.ca>
2024-11-26 16:45:01 +0100

    chore: PR review

--- components/MouseTooltip.tsx
+++ components/MouseTooltip.tsx
@@ -9,16 +9,6 @@ export default function MouseTooltip({
   tooltip: React.ReactNode;
   children: React.ReactNode;
 }) {
-  /* <div
-            tabIndex={-1}
-            className="max-w-56 border bg-background px-2 py-1.5 text-xs rounded-md"
-            {...attrs}
-          >
-            {tooltip}
-          </div> */
-
-  // 1. Tooltip. https://floating-ui.com/docs/tooltip
-  // 2. Follow mouse. https://floating-ui.com/docs/useclientpoint
   return (
     <>
       <div className="hidden">{tooltip}</div>
