866717bcd4aac26bac17a8af032112ac21ac9472
plettj <jlplett@uwaterloo.ca>
2024-12-31 14:45:53 -0800

    update: Photography page

--- scripts/generateBlurData/index.js
+++ scripts/generateBlurData/index.js
@@ -49,40 +49,49 @@ const PATH_PHOTOGRAPHY_IMAGES = "/images/photography";
 
 const photos = [
   {
-    src: `${PATH_PHOTOGRAPHY_IMAGES}/Sigriswil_Panorama_Bridge.jpg`,
-    alt: "The extremely fogged Sigriswil Panorama Bridge from the show Crash Landing on You in Interlaken, Switzerland",
-    location: "Sigriswil, Switzerland",
+    src: `${PATH_PHOTOGRAPHY_IMAGES}/Andermatt_City_Outlook.jpg`,
+    alt: "A bright little valley city in the middle of the snow-smothered Swiss Alps.",
+    location: "Andermatt, Switzerland",
     year: "2024",
-    size: [2268, 4032],
-  },
-
-  {
-    src: `${PATH_PHOTOGRAPHY_IMAGES}/Alsace_Haut-Koenigsbourg_Castle.jpg`,
-    alt: "A hilltop view above the fog of the sprawling French hills from Haut-Koenigsbourg Castle in Alsace",
-    location: "Alsace, France",
-    year: "2024",
-    size: [2863, 1606],
+    size: [3494, 1964],
+    blurDataURL:
+      "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAHAAwDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAQF/8QAIBAAAQMEAgMAAAAAAAAAAAAAAQIDBAAFEiERMQYTgf/EABQBAQAAAAAAAAAAAAAAAAAAAAP/xAAXEQADAQAAAAAAAAAAAAAAAAAAAQIS/9oADAMBAAIRAxEAPwCy3eaSblIQu+yovqc2Ww2vIa6IAx+g1oyWLYH1ZuvJUd4oA4HOx2mlKWrYGEf/2Q==",
   },
   {
-    src: `${PATH_PHOTOGRAPHY_IMAGES}/Lauterbrunnen_Jungfrau_Peak_View.jpg`,
-    alt: "Snow-capped peaks, viewed from the top of Jungfrau in the Swiss Alps at Lauterbrunnen",
-    location: "Lauterbrunnen, Switzerland",
+    src: `${PATH_PHOTOGRAPHY_IMAGES}/Chur_Panorama_Train_View.jpg`,
+    alt: "A quaint bridge over a little valley in the frosted mountain forests of central Switzerland.",
+    location: "Chur, Switzerland",
     year: "2024",
-    size: [4032, 2268],
+    size: [3658, 2191],
+    blurDataURL:
+      "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAHAAwDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAUG/8QAIRAAAQMEAgMBAAAAAAAAAAAAAQIDEQAEBhIFITFBUZH/xAAUAQEAAAAAAAAAAAAAAAAAAAAD/8QAGBEAAwEBAAAAAAAAAAAAAAAAAAERQVH/2gAMAwEAAhEDEQA/AJWF49zToed4jlbi0ebTKxvAJImBH52PVax7JMrBSDfshQGqtxqZHR6SCPI+0pSK9CieH//Z",
   },
   {
-    src: `${PATH_PHOTOGRAPHY_IMAGES}/Lauterbrunnen_Valley_Fog.jpg`,
-    alt: "The Lauterbrunnen Valley with sun-kissed fog in the Swiss Alps",
-    location: "Lauterbrunnen, Switzerland",
+    src: `${PATH_PHOTOGRAPHY_IMAGES}/Zermatt_Matterhorn.jpg`,
+    alt: "The iconic Matterhorn mountain peak, standing alone in the clouds.",
+    location: "Zermatt, Switzerland",
     year: "2024",
-    size: [2268, 3376],
+    size: [4032, 1371],
+    blurDataURL:
+      "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAEAAwDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAYH/8QAHhAAAgICAgMAAAAAAAAAAAAAAQIAAwQFEVESITH/xAAVAQEBAAAAAAAAAAAAAAAAAAADBP/EABURAQEAAAAAAAAAAAAAAAAAAAAB/9oADAMBAAIRAxEAPwDaLthk26+ktaQ1ihiR9BPUkK9tksbBYUcq5XyI9njviIlEBX//2Q==",
   },
   {
-    src: `${PATH_PHOTOGRAPHY_IMAGES}/Ribeauville_Alley.jpg`,
-    alt: "A mild but posh residential alleyway in Ribeauvillé, Alsace",
-    location: "Ribeauvillé, France",
+    src: `${PATH_PHOTOGRAPHY_IMAGES}/Zurich_ETH_Staircase.jpg`,
+    alt: "A little spiral of history in the heart of ETH Zürich University campus.",
+    location: "Zürich, Switzerland",
     year: "2024",
-    size: [2268, 3818],
+    size: [2186, 2186],
+    blurDataURL:
+      "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAMAAwDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAABgQH/8QAIxAAAQQBAwQDAAAAAAAAAAAAAQIDBBEhAAUGEhMjMUFh4f/EABQBAQAAAAAAAAAAAAAAAAAAAAP/xAAYEQEAAwEAAAAAAAAAAAAAAAABABESAv/aAAwDAQACEQMRAD8ApbgMuOS5u5dtTbKAt5T6iVKJyKr1+jGsu5vucOfySS9txDMSkhtDj60kYF4Fj3elPKXVbrAZekEpX2wPGSnFnBzn186BBa4w6GVdIJJNpBs2ReR9aF5w0MW9E//Z",
+  },
+  {
+    src: `${PATH_PHOTOGRAPHY_IMAGES}/Zurich_Skyline.jpg`,
+    alt: "The Zürich cityscape, with its iconic church spires and vast mountain backdrop.",
+    location: "Zürich, Switzerland",
+    year: "2024",
+    size: [2106, 3742],
+    blurDataURL:
+      "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAVAAwDASIAAhEBAxEB/8QAGQAAAgMBAAAAAAAAAAAAAAAAAAcCBAUI/8QAJBAAAgEDAwMFAAAAAAAAAAAAAQIDAAQFERIhBhNhBzFBUXH/xAAUAQEAAAAAAAAAAAAAAAAAAAAD/8QAGhEBAAIDAQAAAAAAAAAAAAAAAQARAgNRIf/aAAwDAQACEQMRAD8A6Ykugo1NRF+tJy56+zEYkN1j7aLssHbtyiQeEPsQT+VQf1JyDbXVGAYa6JEGA8U1Y9he8i3xmNN50vPkY5zBJE2yQIOZND966jnn5rIGRu4UTs3Vyiuu7aJTxRRQalVGNmUFT//Z",
   },
 ];
 
