22d4a2883cc009f261fc701d526448f40e1e3d23
plettj <jlplett@uwaterloo.ca>
2025-01-18 19:56:05 -0800

    update: Add go engine

--- lib/constants.ts
+++ lib/constants.ts
@@ -19,8 +19,8 @@ export const URL_MY_GITHUB = "https://github.com/plettj";
 
 export const COPYRIGHT_STRING = `© ${new Date().getFullYear()} Josiah Plett`;
 
-export const VISITOR_EXPIRATION = 60 * 60 * 24;
-export const FALLBACK_TOTAL_VISITORS = 679; // NOTE: Old website had 1080 views as of 2024-10-28.
+export const VISITOR_EXPIRATION = 60 * 60 * 24; // Visits are considered unique again after 24 hours.
+export const FALLBACK_TOTAL_VISITORS = 2587; // NOTE: Old website had 1080 unique visits as of 2024-10-28.
 
 export const META_TITLE_HOME = "Josiah Plett";
 export const META_DESCRIPTION_HOME =
@@ -28,6 +28,7 @@ export const META_DESCRIPTION_HOME =
 export const META_DESCRIPTION_NOTES =
   "Josiah Plett's university course notes. Distribute freely with attribution.";
 
+/* Only used for metadata */
 export const THEME_BACKGROUND = "#FFFFFF";
 export const THEME_LIGHT_PRIMARY = "#1C1917";
 export const THEME_DARK_PRIMARY = "#FAFAF9";
